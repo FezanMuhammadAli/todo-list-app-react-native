aa61dd780eebf1511643ec75e5fe4a6ed4f6cab1
Fezan Muhammad Ali <callmetheengcountio@gmail.com>
2024-01-07 08:20:30 +0500

    Add today's tasks section also a reusable component Task.js privious code is updated. added a static layout of the tasks.

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Here Goes FEZAN!</Text>
-      <Text>Mark It As A BoilerPlate Code 😒</Text>
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+
+          <Task text={"This is a task 1."} />
+          <Task text={"This is a task 2."} />
+          <Task text={"This is a task 3."} />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#E8EaEd",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
